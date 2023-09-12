d8737bbb4cb6eac300fb3b6b31fefcdebdbcc1d3
Volodymyr Bondar <2005volka@gmail.com>
2023-09-12 17:25:48 +0300

    done
    done

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,8 +1,10 @@
 const { User } = require("../../models");
 const { userSchema } = require("../../schemas");
+const sendEmail = require("../../helpers/sendEmail");
 
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
 
 const register = async (req, res, next) => {
   try {
@@ -25,13 +27,24 @@ const register = async (req, res, next) => {
       });
       return;
     }
+    const verificationToken = uuidv4();
     const avatarURL = gravatar.url(email, { d: "mp" });
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
     const result = await User.create({
       email,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    const mail = {
+      to: email,
+      subject: "Please Verify Your Identity",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Verify Your Identity</a>`,
+    };
+
+    await sendEmail(mail);
+
     res.status(201).json({
       status: "Created",
       code: 201,
@@ -40,6 +53,7 @@ const register = async (req, res, next) => {
           email,
           subscription: result.subscription,
           avatarURL,
+          verificationToken,
         },
       },
     });
